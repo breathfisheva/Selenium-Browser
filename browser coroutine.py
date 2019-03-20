9dd44eba1ee8c73bc7607cf398331a9ad09ba222
breathfisheva <hongliu_goodluck@sina.com>
2019-03-20 14:33:41 +0800

    coroutine

--- browser coroutine.py
+++ browser coroutine.py
@@ -1,4 +1,9 @@
+'''
+使用协程实现异步
+'''
+
 from  selenium import webdriver
+import asyncio
 
 browsers = ['Firefox', 'Chrome', 'IE', 'Edge']
 
@@ -12,8 +17,18 @@ def init_webdriver(broswer):
     elif broswer == 'Edge':
         return webdriver.Edge()
 
-for broswer in browsers:
+@asyncio.coroutine
+def visi_website(broswer):
     driver = init_webdriver(broswer)
+    yield from asyncio.sleep(1) #必须用异步，否则即使用了coroutine也没法实现异步
     driver.get('https://www.baidu.com')
     driver.close()
+
+loop = asyncio.get_event_loop()
+tasks = []
+for browser in browsers:
+    tasks.append(visi_website(browser))
+print(tasks)
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
 
